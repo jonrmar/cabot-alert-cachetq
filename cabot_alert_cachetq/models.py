e78be461b946a7fc94a11afbfcf1f9caac9a14d7
Jonathan <jonathan@ubots.com.br>
2019-01-04 18:24:51 -0200

    adding logs to models.py

--- cabot_alert_cachetq/models.py
+++ cabot_alert_cachetq/models.py
@@ -1,6 +1,6 @@
 from cabot.cabotapp.alert import AlertPlugin, AlertPluginUserData
 from os import environ as env
-
+from pprint import pprint
 from logging import getLogger
 logger = getLogger(__name__)
 
@@ -14,7 +14,12 @@ class CachetqAlertPlugin(AlertPlugin):
 #    cachetq_aliases = [u.cachetq_alias for u in CachetqAlertUserData.objects.filter(user__user__in=users)]
 
     def send_alert(self, service, users, duty_officers):
-	logger.info('Infos: Self: {}, Service: {}, Users: {}, Duty_Officers: {}'.format(self, service, users, duty_officers))
+	logger.info('Beginning Test')
+        pprint(vars(self))
+        pprint(vars(service))
+        pprint(vars(users))
+        pprint(vars(duty_officers))
+        logger.info('Infos: Self: {}, Service: {}, Users: {}, Duty_Officers: {}'.format(self, service, users, duty_officers))
         for u in users:
             logger.info('This is bad for your {}.'.format(
                 u.cabot_alert_cachetq_settings.favorite_bone))
