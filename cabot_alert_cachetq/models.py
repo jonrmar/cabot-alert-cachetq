9a09c781b0fd4f46f6fcfd70fcb5366d957a88b0
Jonathan <jonathan@ubots.com.br>
2019-01-16 18:08:09 -0200

    validating status before creating cachet incident

--- cabot_alert_cachetq/models.py
+++ cabot_alert_cachetq/models.py
@@ -42,7 +42,7 @@ class CachetqAlertPlugin(AlertPlugin):
                 incident_id = incidents[0]['id']
                 if incidents[0]['status'] != 4:
                     self._update_cachetq_incident(message, service, component_status, component_id, incident_id)
-            else:
+            elif service.overall_status != service.PASSING_STATUS:
                 self._create_cachetq_incident(message, service, component_status, component_id)
         else:
             logger.warning('Service not found in config: %s', service.name)
