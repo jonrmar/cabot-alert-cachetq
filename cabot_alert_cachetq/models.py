c7df77173d9a693f6d4d2f01f573681640832cb1
Jonathan <jonathan@ubots.com.br>
2019-01-17 11:55:55 -0200

    refactoring code

--- cabot_alert_cachetq/models.py
+++ cabot_alert_cachetq/models.py
@@ -2,6 +2,7 @@ from cabot.cabotapp.alert import AlertPlugin, AlertPluginUserData
 from os import environ as env
 from pprint import pprint
 from logging import getLogger
+from enum import Enum
 from django.template import Context, Template
 
 import requests
@@ -12,12 +13,19 @@ logger = getLogger(__name__)
 cachetq_template="""
 Service {{ service.name }} {% if service.overall_status == service.PASSING_STATUS %} is back to normal {% else %}\ 
 reporting *{{ service.overall_status }}* status: {% endif %}\
-{% if service.overall_status != service.PASSING_STATUS %}Checks failing: \
+{% if service.overall_status != service.PASSING_STATUS %} Checks failing: \
 {% for check in service.all_failing_checks %}\
     - {{ check.name }}
 {% endfor %}\
 {% endif %}
 """    
+
+class Visible(Enum):
+    ALL = 1
+
+class Status(Enum): 
+    INVESTIGATING = 1
+    FIXED = 4
 
 class CachetqAlertPlugin(AlertPlugin):
     name = "Cachetq Alert"
@@ -32,21 +40,23 @@ class CachetqAlertPlugin(AlertPlugin):
         component_status = json.loads(env.get('CACHETQ_COMPONENT_STATUS'))
 
         if service.name in component_id:
-            incidents = self._get_cachetq_incidents(service, component_id)
-
-            c = Context({ 'service': service })
-            message = Template(cachetq_template).render(c)
-
-            if incidents:
-                incident_id = incidents[0]['id']
-                if incidents[0]['status'] != 4:
-                    self._update_cachetq_incident(message, service, component_status, component_id, incident_id)
-                elif service.overall_status != service.PASSING_STATUS:
-                    self._create_cachetq_incident(message, service, component_status, component_id)   
-            elif service.overall_status != service.PASSING_STATUS:
-                self._create_cachetq_incident(message, service, component_status, component_id)
+            self._manage_cachetq_incidents(service, component_id, component_status)
         else:
             logger.warning('Service not found in config: %s', service.name)
+
+    def _manage_cachetq_incidents(self, service, component_id, component_status):
+        incidents = self._get_cachetq_incidents(service, component_id)
+
+        context_template = Context({ 'service': service })
+        message = Template(cachetq_template).render(context_template)
+
+        if incidents:
+            if incidents[0]['status'] != Status.FIXED.value:
+                self._update_cachetq_incident(message, service, component_status, component_id, incidents[0])
+            elif service.overall_status != service.PASSING_STATUS:
+                self._create_cachetq_incident(message, service, component_status, component_id)      
+        elif service.overall_status != service.PASSING_STATUS:
+            self._create_cachetq_incident(message, service, component_status, component_id)
 
     def _create_cachetq_incident(self, message, service, component_status, component_id):      
         logger.info('Creating Cachetq Alert to: %s', service.overall_status)
@@ -58,26 +68,28 @@ class CachetqAlertPlugin(AlertPlugin):
             data=json.dumps({
                 'name': service.name,
                 'message': message,
-                'status': 1, #Investigating
-                'visible': 1,
+                'status': Status.INVESTIGATING.value,
+                'visible': Visible.ALL.value, 
                 'component_id': component_id[service.name],
                 'component_status': component_status[service.overall_status],
                 'notify': True
             })
         )
        
-    def _update_cachetq_incident(self, message, service, component_status, component_id, incident_id):
+    def _update_cachetq_incident(self, message, service, component_status, component_id, incident):
     	logger.info('Updating Cachetq Alert to: %s', service.overall_status)
-                
-        url = env.get('CACHETQ_URL') + "/incidents/{id}".format( id=incident_id )
+
+        incident_status = Status.FIXED.value if service.overall_status == service.PASSING_STATUS else incident['status']          
+        url = env.get('CACHETQ_URL') + "/incidents/{id}".format( id=incident['id'])
         headers = {'Content-Type':'application/json', 'X-Cachet-Token': env.get('CACHETQ_TOKEN')}
+        
         requests.put(url, 
             headers=headers,
             data=json.dumps({
                 'name': service.name,
                 'message': message,
-                'status': 4, #Fixed
-                'visible': 1,
+                'visible': Visible.ALL.value,
+                'status': incident_status,
                 'component_id': component_id[service.name],
                 'component_status': component_status[service.overall_status],
                 'notify': True
